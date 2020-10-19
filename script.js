d0e1c17087696e947dd51c1772106ce66c13999b
joe riggs <josephariggs@gmail.com>
2020-10-18 21:59:07 -0400

    sounds1

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 let sound = {
-    init : () => 
-        {
-            document.addEventListener('click', handleClick);
-            window.addEventListener('keydown', performClick);
-        },
+    init: () => {
+        document.addEventListener('click', handleClick);
+        window.addEventListener('keydown', performClick);
+    },
 
-
+    library = 1
 }
 
-let library = 1;
-
-function performClick(event){
+function performClick(event) {
 
     let element = document.getElementById(event.key);
     element.click();
@@ -19,27 +16,20 @@ function performClick(event){
 
 function handleClick(event) {
     playSound(event.target.id);
-
-	// If the clicked element doesn't have the right selector, bail
-
-	// Don't follow the link
-	event.preventDefault();
-console.log(event.target.id);
-	// Log the clicked element in the console
     event.target.classList.add("active");
     removeClass(event.target);
 };
 
 
-function playSound (number) {
+function playSound(number) {
     let x = document.getElementById("play" + number)
-    x.currentTime = 0; 
+    x.currentTime = 0;
     x.play();
 }
 
-function removeClass(target){
+function removeClass(target) {
     setTimeout(
-        ()=>{
-        target.classList.remove("active");
+        () => {
+            target.classList.remove("active");
         }, 100);
 }
